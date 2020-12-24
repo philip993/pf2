9bb0737abaece6cf1fda63629cb3efeb004bd6a8
Filip FICO <fitz993m@gmail.com>
2020-12-24 01:17:40 +0100

    1.4. Index page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import IndexImage from '../components/IndexImage/IndexImage';
+import IndexText from '../components/IndexText';
+import styles from '../styles/index.module.scss';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <IndexText />
+      <IndexImage />
+    </div>
+  );
+}
