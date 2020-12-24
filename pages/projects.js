f22367d9c2efbb543327971e2201e6596bb510f0
Filip Mihajlovski <fitz993m@gmail.com>
2020-12-24 15:40:12 +0100

    1.9. Project page

---
+++ pages/projects.js
@@ -0,0 +1,34 @@
+import styles from "../styles/projects.module.scss";
+
+const Projects = () => {
+  return (
+    <div className={styles.projectContainer}>
+      <div className={styles.projectFirstContainer}>
+        <span className={styles.spanOne}>
+          <h1>
+            E-Commerce
+            <a href="/">E-Commerce</a>
+          </h1>
+        </span>
+        <span className={styles.spanOne}>
+          <h1>
+            OSM-Maps
+            <a href="/">OSM-Maps</a>
+          </h1>
+        </span>
+        <span className={styles.spanOne}>
+          <h1>
+            Employee Records <a href="/">Employee Records</a>
+          </h1>
+        </span>
+        <span className={styles.spanOne}>
+          <h1>
+            Accounting <a href="/">Accounting </a>
+          </h1>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
