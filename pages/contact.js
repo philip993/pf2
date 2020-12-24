a154fd5c27ac01dc36c2896528490701edd06611
Filip Mihajlovski <fitz993m@gmail.com>
2020-12-24 15:39:36 +0100

    1.8. Contact page

---
+++ pages/contact.js
@@ -0,0 +1,40 @@
+import styles from "../styles/contact.module.scss";
+
+const Contact = () => {
+  return (
+    <div className={styles.contactContainer}>
+      <div className={styles.contactFirstContainer}>
+        <h1>Feel Free to Contact Me!</h1>
+      </div>
+      <div className={styles.contactSecondContainer}>
+        <h1>Contact</h1>
+        <p>
+          I'm looking for an opportunity to work with you. You can contact me on
+          the following platforms.
+        </p>
+        <span className={styles.spanOne}>
+          <h1>
+            filipmihajlovski.me
+            <a href="http://filipmihajlovski.me">filipmihajlovski.me</a>
+          </h1>
+        </span>
+        <span className={styles.spanOne}>
+          <h1>
+            LinkedIn
+            <a href="https://linkedin.com/in/filip-mihajlovski-68773116b">
+              LinkedIn
+            </a>
+          </h1>
+        </span>
+        <span className={styles.spanOne}>
+          <h1>
+            GitHub{" "}
+            <a href="https://github.com/philip993?tab=repositories">GitHub</a>
+          </h1>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
