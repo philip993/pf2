03343902395f462f9909c9c33cf9bacdb9bd59f5
Filip Mihajlovski <fitz993m@gmail.com>
2020-12-25 12:54:41 +0100

    1.18. E-commerce project

--- pages/ecommerce.js
+++ pages/ecommerce.js
@@ -1,4 +1,8 @@
-import styles from '../styles/ecommerce.module.scss';
+import styles from "../styles/ecommerce.module.scss";
+import MongoLogo from "../public/mongodb.svg";
+import ExpressLogo from "../public/expressjs.svg";
+import ReactLogo from "../public/react-js.svg";
+import NodeLogo from "../public/nodejs.svg";
 
 const Ecommerce = () => {
   return (
@@ -11,12 +15,26 @@ const Ecommerce = () => {
       <div className={styles.ecommerceThirdContainer}>
         <h1>Overview</h1>
         <p>
-          E-Commerce is a project in which I created an online Bookstore.
-          Protected the webstore with User Authorization and Authentication.
+          The first big project I created was an online Bookstore, called
+          E-Commerce. There are four types or Personas, including Visitor,
+          Customer, Admin and Super-Admin. Customer can look through the Books
+          and buy or just save one for later on. Order system included, as well
+          as Admin Panel.
         </p>
+        <p>
+          From the Technology stack, I used MERN stack with external payment
+          system - Stripe.
+        </p>
+        <div className={styles.techStackContainer}>
+          <img src={MongoLogo} alt="mongo" />
+          <img src={ExpressLogo} alt="express" />
+          <img src={ReactLogo} alt="react" />
+          <img src={NodeLogo} alt="node" />
+        </div>
       </div>
       <div className={styles.ecommerceFourthContainer}>
         <h1>Problem Definitions</h1>
+        <p>In this project I defined two main problem.</p>
         <p>
           First problem defined in this project to restrict access of users and
           visitors to certain pages. Also to allow only logged-in user option to
@@ -24,26 +42,31 @@ const Ecommerce = () => {
         </p>
         <p>
           Second problem I encountered was the need for integration between
-          Stripe and my Back-end code.
+          Stripe and my Back-end code and keeping DRY principle.
         </p>
       </div>
       <div className={styles.ecommerceFifthContainer}>
         <h1>Solution</h1>
         <p>
-          I implemented user Authorization via admin attribute on all users. By
-          default this field is set to false and only Super-Admin can give Admin
-          roles to another user. As for the second part, I created encryption
-          for User Password on registration, as well as decryption on login.
-          Also I implemented a JWT system, with token of maximum one hour time
-          before loging out.
+          As a solution for the first problem, I implemented user Authorization
+          via admin attribute on all users. By default this field is set to
+          false and only Super-Admin can give Admin roles to another user. As
+          for the second part, I created encryption for User Password on
+          registration, as well as decryption on login. Also I implemented a JWT
+          system, with token of maximum one hour time before loging out.
         </p>
         <p>
-          In order to use Stripe, I had to create Product for each Book, that
-          has inventory (SKU). Also for each User, there is a Customer created
-          in Stripe. Thus means doubling work for each action. Using async
-          functions helped me create these request via Client-side UI. For e.g.
-          when the user registers, he creates User in Database and also a
-          Customer in Stripe.
+          In order to use Stripe in my project, a complete integration is
+          required between Stripe and back-end logic. Stripe has its own unique
+          system, that requires a Product/Service with defined inventory (SKU).
+          Customer is created when payment is done and can be used for multiple
+          payments or just one. Stripe has Order system, that uses Products, SKU
+          and customer. In order to create all of this, plus the code on
+          server-side, I had to find a way to keep DRY principle. I started with
+          Async/Await functions, while creating a Book by an Admin in the Admin
+          Panel to the Database, subsequently creating SKU and followed by
+          Product in Stripe. With this approach I successfully eliminated the
+          second problem.
         </p>
       </div>
       <div className={styles.ecommerceSixthContainer}>
