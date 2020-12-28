81f05f1667c8f04cf772197fcddb31894de021ed
Filip FICO <fitz993m@gmail.com>
2020-12-28 13:23:12 +0100

    1.28. Change URL

--- pages/projects.js
+++ pages/projects.js
@@ -1,29 +1,31 @@
-import styles from "../styles/projects.module.scss";
+import Layout from '../components/Layout/Layout';
+import styles from '../styles/projects.module.scss';
 
 const Projects = () => {
   return (
     <div className={styles.projectContainer}>
+      <Layout />
       <div className={styles.projectFirstContainer}>
         <span className={styles.spanOne}>
           <h1>
             E-Commerce
-            <a href="/ecommerce">E-Commerce</a>
+            <a href="/projects/ecommerce">E-Commerce</a>
           </h1>
         </span>
         <span className={styles.spanOne}>
           <h1>
             OSM-Maps
-            <a href="/osm">OSM-Maps</a>
+            <a href="/projects/osm">OSM-Maps</a>
           </h1>
         </span>
         <span className={styles.spanOne}>
           <h1>
-            Employee Records <a href="/">Employee Records</a>
+            Employee Records <a href="/projects/records">Employee Records</a>
           </h1>
         </span>
         <span className={styles.spanOne}>
           <h1>
-            Accounting <a href="/">Accounting </a>
+            Accounting <a href="/projects/accounting">Accounting </a>
           </h1>
         </span>
       </div>
